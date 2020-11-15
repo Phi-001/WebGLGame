d58d1821a47ffc31c6b014f9122bfe618b1563d4
Phi-001 <shusan.california@gmail.com>
2020-11-15 12:29:50 -0800

    Actual Collision

--- JS/main.js
+++ JS/main.js
@@ -118,92 +118,92 @@ window.addEventListener("keyup", (e) => {
     keys[code] = false;
 });
 
+function atLeastTwo(a, b, c) {
+    return a ? (b || c) : (b && c);
+}
+
 function collision(camera, level, dir) {
-    let x = camera.position[0] + dir[0] * camera.stepSize;
-    let y = camera.position[1] + dir[1] * camera.stepSize;
-    let z = camera.position[2] + dir[2] * camera.stepSize;
-    for (let i = 0; i < level.length; i += 12) {
-        const xmax = Math.max(level[i    ], level[i + 3], level[i + 6]),
-              ymax = Math.max(level[i + 1], level[i + 4], level[i + 7]),
-              zmax = Math.max(level[i + 2], level[i + 5], level[i + 8]),
-              xmin = Math.min(level[i    ], level[i + 3], level[i + 6]),
-              ymin = Math.min(level[i + 1], level[i + 4], level[i + 7]),
-              zmin = Math.min(level[i + 2], level[i + 5], level[i + 8]);
-        if (level[i] === level[i + 3] && level[i + 3] === level[i + 6] && y < ymax && y > ymin && z > -zmax && z < -zmin) {
-            const py = level[i + 4] - level[i + 1],
-                  qy = level[i + 7] - level[i + 1],
-                  pz = level[i + 5] - level[i + 2],
-                  qz = level[i + 8] - level[i + 2],
-                  sign = py * qz - pz * qy;
-            if (sign < 0) {
-                x = Math.max(x, level[i] + 0.1);
-            } else {
-                x = Math.min(x, level[i] - 0.1);
-            }
-        } else if (level[i + 1] === level[i + 4] && level[i + 4] === level[i + 7] && x < xmax && x > xmin && z > -zmax && z < -zmin) {
-            const px = level[i + 3] - level[i],
-                  qx = level[i + 6] - level[i],
-                  pz = level[i + 5] - level[i + 2],
-                  qz = level[i + 8] - level[i + 2],
-                  sign = pz * qx - px * qz;
-            if (sign < 0) {
-                y = Math.max(y, level[i + 1] + 0.1);
-            } else {
-                y = Math.min(y, level[i + 1] - 0.1 - player.height);
-            }
-        } else if (level[i + 2] === level[i + 5] && level[i + 5] === level[i + 8] && x < xmax && x > xmin && y < ymax && y > ymin) {
-            const py = level[i + 3] - level[i],
-                  qy = level[i + 6] - level[i],
-                  pz = level[i + 4] - level[i + 1],
-                  qz = level[i + 7] - level[i + 1],
-                  sign = py * qz - pz * qy;
-            if (sign < 0) {
-                z = Math.min(z, -level[i + 2] - 0.1);
-            } else {
-                z = Math.max(z, -level[i + 2] + 0.1);
-            }
+    const normals = level.normals;
+    const vertices = level.vertices;
+    const vx = dir[0] * camera.stepSize;
+    const vy = dir[1] * camera.stepSize;
+    const vz = dir[2] * camera.stepSize;
+    const x = camera.position[0];
+    const y = camera.position[1];
+    const z = camera.position[2];
+    for (let i = 0; i < vertices.length; i += 12) {
+        const normalx = normals[i];
+        const normaly = normals[i + 1];
+        const normalz = normals[i + 2];
+        const d = -(vertices[i] * normalx + vertices[i + 1] * normaly - vertices[i + 2] * normalz);
+        const t = -(normalx * x + normaly * y + normalz * z + d - 0.1) / (normalx * vx + normaly * vy + normalz * vz);
+        if (t < 0 || t >= 1 || !t) {
+            continue;
+        }
+        const xmax = Math.max(vertices[i], vertices[i + 3], vertices[i + 6]);
+        const ymax = Math.max(vertices[i + 1], vertices[i + 4], vertices[i + 7]);
+        const zmax = Math.max(vertices[i + 2], vertices[i + 5], vertices[i + 8]);
+        const xmin = Math.min(vertices[i], vertices[i + 3], vertices[i + 6]);
+        const ymin = Math.min(vertices[i + 1], vertices[i + 4], vertices[i + 7]);
+        const zmin = Math.min(vertices[i + 2], vertices[i + 5], vertices[i + 8]);
+        const newx = x + t * vx + Number.EPSILON * normalx;
+        const newy = y + t * vy + Number.EPSILON * normaly;
+        const newz = z + t * vz + Number.EPSILON * normalz;
+        if (atLeastTwo(newx <= xmax && newx >= xmin, newz >= -zmax && newz <= -zmin, newy <= ymax && newy >= ymin)) {
+            return [newx, newy, newz];
         }
     }
-    return [x, y, z];
+    return [x + vx, y + vy, z + vz];
 }
 
 function draw() {
     if (keys["KeyW"]) {
-        const newPos = collision(camera, levels[0].vertices, [-camera.dir[0], 0, camera.dir[2]]);
+        const newPos = collision(camera, levels[0], [-camera.dir[0], 0, camera.dir[2]]);
         camera.position[0] = newPos[0];
+        camera.position[1] = newPos[1];
         camera.position[2] = newPos[2];
     }
     if (keys["KeyA"]) {
-        const newPos = collision(camera, levels[0].vertices, [camera.dir[2], 0, camera.dir[0]]);
+        const newPos = collision(camera, levels[0], [camera.dir[2], 0, camera.dir[0]]);
         camera.position[0] = newPos[0];
+        camera.position[1] = newPos[1];
         camera.position[2] = newPos[2];
     }
     if (keys["KeyS"]) {
-        const newPos = collision(camera, levels[0].vertices, [camera.dir[0], 0, -camera.dir[2]]);
+        const newPos = collision(camera, levels[0], [camera.dir[0], 0, -camera.dir[2]]);
         camera.position[0] = newPos[0];
+        camera.position[1] = newPos[1];
         camera.position[2] = newPos[2];
     }
     if (keys["KeyD"]) {
-        const newPos = collision(camera, levels[0].vertices, [-camera.dir[2], 0, -camera.dir[0]]);
+        const newPos = collision(camera, levels[0], [-camera.dir[2], 0, -camera.dir[0]]);
         camera.position[0] = newPos[0];
+        camera.position[1] = newPos[1];
         camera.position[2] = newPos[2];
     }
     if (keys["Space"] && player.onGround) {
-        const newPos = collision(camera, levels[0].vertices, [0, -5, 0]);
+        const newPos = collision(camera, levels[0], [0, -5, 0]);
+        camera.position[0] = newPos[0];
         camera.position[1] = newPos[1];
+        camera.position[2] = newPos[2];
         player.onGround = false;
     }
     if (keys["ShiftLeft"]) { 
-        const newPos = collision(camera, levels[0].vertices, [0, 1, 0]);
+        const newPos = collision(camera, levels[0], [0, 1, 0]);
+        camera.position[0] = newPos[0];
         camera.position[1] = newPos[1];
+        camera.position[2] = newPos[2];
     }
 
-    const gravity = collision(camera, levels[0].vertices, [0, 0.2, 0]);
+    const gravity = collision(camera, levels[0], [0, 0.2, 0]);
     if (camera.position[1] === gravity[1]) {
         player.onGround = true;
     } else {
-        camera.position[1] = gravity[1];
+        player.onGround = false;
     }
+    camera.position[0] = gravity[0];
+    camera.position[1] = gravity[1];
+    camera.position[2] = gravity[2];
 
     clear(gl);
 
